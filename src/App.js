c0c5c8dc67c4a1a1d9626a7ae05c76a45cbbb45e
Mohamed Ali <76604718+mohamedali12317@users.noreply.github.com>
2023-01-24 00:30:49 +0200

    explan new things abot problem

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 function App() {
   return (
     <>
-    {/*  */}
+    {/* creat new brench
+
+      if i dont have node_modules i will make npm i it will make the follders 
+      and if i have promblem again 
+      i will delet the folder of eslint-config-react-app
+      or we can try the file first of base.js in the folder of eslint-config-react-app
+      
+      and i have made this code too when i had problem npm run build
+      
+      https://create-react-app.dev/docs/available-scripts/#npm-run-eject
+
+    */}
+
       <h1 className="title">frist project with react</h1>
       <h2>mohamed ali</h2>
     </>
